7850ba60368ad88ecf33d284199c7d4980e59e0c
Jpczxc <jpc.mdjay@gmail.com>
2024-06-11 00:51:36 +0800

    import js

--- src/Camazon.js
+++ src/Camazon.js
@@ -3,6 +3,9 @@ import Slider from './components/Slider';
 import Items from './components/Items';
 import Footer from './components/Footer';
 import 'bootstrap/dist/css/bootstrap.css';
+// import 'jquery/dist/jquery.min.js';
+import 'bootstrap/dist/js/bootstrap.min.js';
+
 
 function Camazon() {
     return(
